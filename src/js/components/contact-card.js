55201a5de105d71e00e2e1d65ee77635512b634a
Andrea Porotti <53380085+andreaporotti@users.noreply.github.com>
2023-01-24 17:02:21 +0100

    Modified card style and structure.

--- src/js/components/contact-card.js
+++ src/js/components/contact-card.js
@@ -1,3 +1,10 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
 export default function ContactCard( attributes ) {
     // List of workplaces.
 	const workplaces = {
@@ -11,10 +18,19 @@ export default function ContactCard( attributes ) {
             { attributes.imageUrl && (
             	<img className="apcc-image" src={ attributes.imageUrl } alt={ attributes.imageAlt } />
             ) }
+
             <div className="apcc-full-name">{ attributes.fullName }</div>
+
+            <span className="apcc-label">{ __( 'Job title', 'ap-contact-card' ) }</span>
             <div className="apcc-job-title">{ attributes.jobTitle }</div>
+
+            <span className="apcc-label">{ __( 'Email', 'ap-contact-card' ) }</span>
             <div className="apcc-email">{ attributes.email }</div>
+
+            <span className="apcc-label">{ __( 'Workplace', 'ap-contact-card' ) }</span>
             <div className="apcc-workplace">{ workplaces[attributes.workplace] }</div>
+
+            <span className="apcc-label">{ __( 'About', 'ap-contact-card' ) }</span>
             <p className="apcc-description">{ attributes.description }</p>
         </>
     )
